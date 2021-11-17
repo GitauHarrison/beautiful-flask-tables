ed8430d8ba97441f36793b8e08d23ef03659dcfc
harry <harrisongitau3@gmail.com>
2021-11-17 12:41:59 +0300

    Add password hashing logic

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
@@ -11,6 +12,12 @@ class User(db.Model):
 
     def __repr__(self):
         return 'User: {}'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Post(db.Model):
