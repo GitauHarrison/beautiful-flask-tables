d112e9926a3427991aa1f64f31faa33f61291450
harry <harrisongitau3@gmail.com>
2021-11-17 13:19:38 +0300

    Require users to login

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
+from werkzeug.urls import url_parse
 from app import app
 from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
@@ -7,8 +8,8 @@ from app.models import User
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {"username": 'Gitau'}
     posts = [
         {
             'author': {'username': 'Rahima'},
@@ -19,7 +20,7 @@ def index():
             'body': 'This is another test'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -33,7 +34,10 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember_me=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
 
