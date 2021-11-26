f8a7e484801ffeea52934380620a1cb233051aa7
harry <harrisongitau3@gmail.com>
2021-11-26 06:48:15 +0300

    Dev: Instantiate faker in the application

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from faker import Faker
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,5 +13,6 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
+faker = Faker(app)
 
 from app import routes, models, errors
