34cc882b175959a36bd43637844bf995be179bd8
harry <harrisongitau3@gmail.com>
2021-11-27 08:26:56 +0300

    Dev: Add environment variables

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from faker import Faker
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,5 +17,24 @@ login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
 faker = Faker(app)
+
+if not app.debug:
+    if app.config['LOG_TO_STDOUT']:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+    else:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/tables.log',
+                                           maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s '
+                '[in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Beautiful Flask Tables')
 
 from app import routes, models, errors
--- config.py
+++ config.py
@@ -3,8 +3,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    # Form 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-cant-guess-this'
+
+    # Database
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
         'postgres://', 'postgresql://') or\
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Logging
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
