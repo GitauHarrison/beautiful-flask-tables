b825da982aaa21bdd7d01b092d4eaf9ed4d57b90
harry <harrisongitau3@gmail.com>
2021-11-28 18:34:56 +0300

    Fix: Set condition for mysql database to accept connection

--- boot.sh
+++ boot.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 source beautiful_flask_tables/bin/activate
-flask db upgrade
+while true; do
+    flask db upgrade
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Upgrade command failed, retrying in 5 secs...
+    sleep 5
+done
+flask translate compile
 exec gunicorn -b :5000 --access-logfile - --error-logfile - table:app
