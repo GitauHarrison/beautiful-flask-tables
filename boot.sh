558c9a26fcab086878ac0daf359028115d931cd7
harry <harrisongitau3@gmail.com>
2021-11-30 16:17:55 +0300

    Fix: Remove flask commands not used

--- boot.sh
+++ boot.sh
@@ -8,5 +8,4 @@ while true; do
     echo Upgrade command failed, retrying in 5 secs...
     sleep 5
 done
-flask translate compile
 exec gunicorn -b :5000 --access-logfile - --error-logfile - table:app
